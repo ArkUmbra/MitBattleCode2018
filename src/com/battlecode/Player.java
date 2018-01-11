067845d902acdd17304fb589c7e88fac049275f8
Luke Gardener <lgardener1337@gmail.com>
2018-01-11 20:19:21 +0000

    First commit of empty player

---
+++ src/com/battlecode/Player.java
@@ -0,0 +1,10 @@
+package com.battlecode;
+
+import bc.GameController;
+
+public class Player {
+
+    public static void main(String args...) {
+        new GameController().asteroidPattern();
+    }
+}
