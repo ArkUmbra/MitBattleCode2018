1324170cb76e8892bd65043852bf50308df18962
Luke Gardener <lgardener1337@gmail.com>
2018-01-11 20:27:41 +0000

    Added boilerplate code for some structure

--- src/com/battlecode/Player.java
+++ src/com/battlecode/Player.java
@@ -4,7 +4,29 @@ import bc.GameController;
 
 public class Player {
 
-    public static void main(String args...) {
-        new GameController().asteroidPattern();
+    public static void main(String... args) {
+        new Player().startMatch();
+    }
+
+    private void startMatch() {
+        while (true) {
+
+            GameController gc = new GameController();
+
+            try {
+                doTurn(gc);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void doTurn(GameController gc) {
+        // do stuff
+        // do stuff
+
+        // Indicate we've finished out turn
+        gc.nextTurn();
     }
 }
